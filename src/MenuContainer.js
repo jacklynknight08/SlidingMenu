5e90298527a2df9d2ba247fe9f4e310c80c06be5
jacklynknight08 <jnk08@outlook.com>
2018-01-14 14:15:49 -0600

    Toggle method created

--- src/MenuContainer.js
+++ src/MenuContainer.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 
 class MenuContainer extends Component {
+    constructor(props, context){
+        super(props, context);
+
+        // Storing a variable called 'visible' in the state object
+        // Creating a toggleMenu method that will be responsible for toggling whether visible is true or false
+        this.state = {
+            visible: false
+        };
+
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    // When this method is called, call toggleMenu that will toggle whether the menu appears or not
+    handleMouseDown(m){
+       this.toggleMenu();
+
+       console.log("Clicked");
+       m.stopPropagation();
+    }
+
+    toggleMenu(){
+        this.setState({
+            visible: !this.state.visible
+        });
+    }
+
     render() {
         return(
             <div>
                 <div>
-                    <p>Can you spot the item that doesn't belong?</p>
+                    <p>List of Colors</p>
                     <ul>
-                        <li>Lorem</li>
-                        <li>Ipsum</li>
-                        <li>Dolor</li>
-                        <li>Sit</li>
-                        <li>Bumblebees</li>
-                        <li>Aenean</li>
-                        <li>Consectetur</li>
+                        <li>Red</li>
+                        <li>Yellow</li>
+                        <li>Blue</li>
+                        <li>Orange</li>
+                        <li>Green</li>
+                        <li>Violet</li>
+                        <li>Indigo</li>
                     </ul>
                 </div>
             </div>
